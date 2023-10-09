1f8f0ee81ffcf1b589c83f70505cefe5a6b7b795
Daniel chuks <danieluchechukwuchuks1@gmail.com>
2023-10-09 16:59:03 +0100

    code structure

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,22 @@
 import type { Metadata } from 'next';
 import { Registry } from '../registry';
+import './global.scss';
 
 export const metadata: Metadata = {
-  title: 'Efass',
-  description: 'Efass app',
+    title: 'Efass',
+    description: 'Efass app',
 };
 
 export default function RootLayout({
-  children,
+    children,
 }: {
-  children: React.ReactNode;
+    children: React.ReactNode;
 }) {
-  return (
-    <Registry>
-      <html lang="en">
-        <body>{children}</body>
-      </html>
-    </Registry>
-  );
+    return (
+        <Registry>
+            <html lang="en">
+                <body className="container">{children}</body>
+            </html>
+        </Registry>
+    );
 }
--- src/page/Home/index.tsx
+++ src/page/Home/index.tsx
@@ -2,7 +2,7 @@ import styles from './index.module.scss';
 
 export const HomePage = () => {
     return (
-        <div className={styles.container}>
+        <div className={styles['container']}>
             <h4>
                 Hello World
             </h4>
