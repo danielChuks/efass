39b6fe931700b27d55c09e7de78c7a8da081f137
Daniel chuks <danieluchechukwuchuks1@gmail.com>
2023-10-11 11:46:08 +0100

    changes

--- src/page/GenerateReport/index.tsx
+++ src/page/GenerateReport/index.tsx
@@ -4,7 +4,13 @@ import styles from './index.module.scss';
 export const GenerateReport = () => {
     return (
         <BaseLayout>
-            <div>Generate Report (Overview)</div>
+            <div className={styles['headerContainer']}>
+                <div>Generate Report (Overview)</div>
+                <div className={styles['timeContainer']}>
+                    <div>Current Date:</div>
+                    <div>July 12, 2023, 12:23 pm</div>
+                </div>
+            </div>
         </BaseLayout>
     );
 };
